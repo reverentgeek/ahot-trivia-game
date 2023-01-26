ab27971830c3d405f142986cae5d06101bc4727d
David Neal <david@reverentgeek.com>
2023-01-26 14:40:44 -0500

    adds montserrat font

--- src/index.js
+++ src/index.js
@@ -26,10 +26,20 @@ server.register( fastifyStatic, {
 	root: publicPath
 } );
 
+// Serve vue.js static files
 const vuePath = path.join( __dirname, "..", "node_modules", "vue", "dist" );
 server.register( fastifyStatic, {
 	root: vuePath,
 	prefix: "/vue/",
+	decorateReply: false // there can be only one!
+	// reply decorator has been added by the first plugin registration
+} );
+
+// Serve fonts
+const fontPath = path.join( __dirname, "..", "node_modules", "@fontsource", "montserrat", "files" );
+server.register( fastifyStatic, {
+	root: fontPath,
+	prefix: "/files/",
 	decorateReply: false // there can be only one!
 	// reply decorator has been added by the first plugin registration
 } );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 "use strict";
 
+const defaultTheme = require( "tailwindcss/defaultTheme" );
+
 module.exports = {
 	content: [ "src/views/*.html", "public/**/*.js" ],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: [
+					"montserrat",
+					...defaultTheme.fontFamily.sans
+				]
+			}
+		},
 	},
 	plugins: [],
 };
