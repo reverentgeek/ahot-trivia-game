a34bcd2fa7bc2f36dbef281db78b84b9e4ff53bd
David Neal <david@reverentgeek.com>
2023-05-18 15:07:58 -0400

    adds triva game graphic

--- public/client.js
+++ public/client.js
@@ -92,7 +92,7 @@ createApp( {
 		} );
 	},
 	template: `
-	<h1 class="text-3xl font-bold text-center">AHOT Trivia Game</h1>
+	<!-- h1 class="text-3xl font-bold text-center">AHOT Trivia Game</!-->
 	<Home v-if="state === 'home'" @join="showJoin" />
 	<Join v-if="state === 'join'" @registerPlayer="registerPlayer" />
 	<Waiting v-if="state === 'waiting'" :playerName="playerName" :id="id" />
